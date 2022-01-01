09780229cf28c57d7116152757619ebe6cfa348c
Git-MrStark <Vivekkumarchandra@gmail.com>
2022-01-01 19:21:50 +0530

    Update Ver1.py

--- Ver1.py
+++ Ver1.py
@@ -4,112 +4,89 @@ import time
 import datetime
 import requests
 import numpy as np
+import csv
 np.set_printoptions(threshold=sys.maxsize)
 channel='@WazirXNotifier'
 
-cryptomat=np.array(['S.No.', 'baseMarket', 'quoteMarket', 'last'])
 
-
-#rows=416
-#column=5
-#cryptomat=[[0]*column]*rows
-#cryptomat[0][0]='quoteMarket'
-
-#print (cryptomat)
-
-
-
-PATH_TO_ADD = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-if PATH_TO_ADD not in sys.path:
-    sys.path.append(PATH_TO_ADD)
-
-from wazirx_sapi_client.rest import Client
-
-# Keys for private events
-api_key = "test_api_key"
-secret_key = "test_secret_key"
-
-# public
-c = Client()
-
-
-#print(type(c.send("ping")))
-#print(type(c.send("time")))
-#print(c.send("system_status"))
-#print(c.send("exchange_info"))
 
 def server_time():
     try:
     	url='https://api.wazirx.com/sapi/v1/time'
     	r=requests.get(url)
     	r=r.json()
-    	t=str(r['serverTime'])
-    	t=t[:-3]
-    	epoch_time=int(t)
-    	datetime_time = datetime.datetime.fromtimestamp(epoch_time)
-    	return datetime_time
+    	t=(r['serverTime'])
+    	t=t/1000
+    	return t
     except KeyError:
     	return server_time()
-    
+    	
+cryptomat=np.array(['S.No.', 'baseAsset', 'quoteAsset', server_time()])
 
+def epoch(t):
+	datetime_time = datetime.datetime.fromtimestamp(t)
+	return datetime_time
+	
+def totimestamp(t):
+	datetime_time=datetime.datetime.timestamp(t)
+	return datetime_time
 
 
 def send(text):
 	url='https://api.telegram.org/bot5043060299:AAF4lNBzk_7N01gNDslg1YjOPBYq9ht0WlE/sendMessage?chat_id='+channel+'&text='+str(text)
 	r=requests.get(url)
 
-		
-def marketstatus():
-	url="https://api.wazirx.com/api/v2/market-status"
-	r=requests.get(url)
-	r=r.json()
-	list=r['markets']
-	global cryptomat
-	#print(len(list))
-	#print((list[0]).keys())
-	total=len(list)
-	i=0
-	while(i!=total):
-		if(list[i]['type']=='SPOT'):
-			#print(i,(list[i])['baseMarket'], (list[i])['quoteMarket'],(list[i])['last'])
-			temp= np.array([i+1,(list[i])['baseMarket'], (list[i])['quoteMarket'],(list[i])['last']])
+
+def market():
+	try:
+		url='https://api.wazirx.com/sapi/v1/tickers/24hr'
+		r= requests.get(url)
+		r=r.json()
+		global cryptomat
+		i=0
+		while(i!=len(r)):
+			temp= np.array([i+1,r[i]['baseAsset'],r[i]['quoteAsset'],r[i]['lastPrice']])
 			cryptomat=np.vstack((cryptomat, temp))
-		i=i+1
-		#print(cryptomat)
-#marketstatus()
+			i=i+1
+	except KeyError:
+		market()
+		
+def pricefetch():
+	try:
+		url='https://api.wazirx.com/sapi/v1/tickers/24hr'
+		r= requests.get(url)
+		r=r.json()
+		global cryptomat
+		temp2= np.array([server_time()])
+		i=0
+		while(i!=len(r)):
+			temp=np.array([r[i]['lastPrice']])
+			temp2=np.vstack([temp2, temp])
+			i=i+1
+		cryptomat=np.hstack([cryptomat,temp2])
+	except KeyError:
+		pricefetch()
+		
 
-def marketstatus2():
-	url="https://api.wazirx.com/api/v2/market-status"
-	r=requests.get(url)
-	r=r.json()
-	list=r['markets']
-	global cryptomat
-	total=len(list)
-	temp2=np.array(['last 2'])
-	i=0
-	while(i!=total):
-		if(list[i]['type']=='SPOT'):
-			temp= np.array((list[i])['last'])
-			temp2=np.vstack((temp2,temp))
-		i=i+1
-	cryptomat=np.hstack((cryptomat,temp2))
 
-#marketstatus2()
+def csvw():
+	array_2D = cryptomat
+	with open("work.csv","w+") as my_csv:
+	  	 newarray = csv.writer(my_csv,delimiter=',')
+	  	 newarray.writerows(array_2D)
+
+
+
+market()
+#print(cryptomat)
+#i=0
+#while(i!=50):
+#	pricefetch()
+#	print(i)
+#	i=i+1
+#pricefetch()
 #print(cryptomat)
 
-def marketstatus3():
-	url="https://api.wazirx.com/api/v2/market-status"
-	r=requests.get(url)
-	r=r.json()
-	list=r['markets']
-	global cryptomat
-	#print(len(list))
-	#print((list[0]).keys())
-	total=len(list)
-	i=40
-	print(i,(list[i])['baseMarket'], (list[i])['quoteMarket'],(list[i])['last'])
-	
-while (1):
-    #marketstatus3()
-    print(server_time())
+#datalog()
 
+csvw()
